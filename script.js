7175c4b73d0882c18250803a3193e4bd1ac0998f
mendi <mendenhall_chris1246@yahoo.com>
2023-06-25 11:03:11 -0700

    finished project

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // Variable to keep track of square count
   let squareCount = 0;
 
+  // Array of random colors
+  let colors = [
+    "#FF0000",
+    "#00FF00",
+    "#0000FF",
+    "#FFFF00",
+    "#FF00FF",
+    "#00FFFF",
+    "#000000",
+  ];
+
   // Create an event listener for when the button is clicked it will create a div
   btn.addEventListener("click", function () {
     // Square Counter
@@ -31,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let div = document.createElement("div");
     // Create a class for the div
     div.className = "blk-square";
+    // Add id to each square
     div.id = squareCount;
     //console.log(squareCount);
 
@@ -38,13 +50,48 @@ document.addEventListener("DOMContentLoaded", function () {
     container.appendChild(div);
     // Event listener for when the mouse is over the square
     div.addEventListener("mouseover", function () {
+      // Change the number color to white
       div.style.color = "white";
+      // Display the ID of the square on the page.
       div.innerText = div.id;
     });
     // Even Listener for when mouse is off square
     div.addEventListener("mouseout", function () {
-      div.style.color = "";
+      // remove the text from the square
       div.innerText = "";
+    });
+
+    // Event Listener to add a random color to the square when clicked
+    div.addEventListener("click", function () {
+      // Set random color to a variable
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      // Add the random color to the square
+      div.style.backgroundColor = randomColor;
+    });
+
+    // Event listener for when the square is double clicked it is to remove squares.
+    div.addEventListener("dblclick", function () {
+      // Check to see if the square id is even.
+      if (div.id % 2 === 0) {
+        // Check to see if there is a next sibling (square)
+        if (div.nextElementSibling) {
+          // Remove next square
+          div.nextElementSibling.remove();
+        } else {
+          // alert if no square is found
+          alert("There is no square to delete");
+        }
+        // check to see if square id is odd
+      } else if (div.id % 2 === 1) {
+        // check to see if therer is a previous sibling (square)
+        if (div.previousElementSibling) {
+          //remove previous sibling (square)
+          div.previousElementSibling.remove();
+        } else {
+          // alert if no square is found
+          alert("There is no square to delete");
+        }
+      }
     });
   });
 
